e4374b656079fbb83a6a7355d8ad882d94f77c92
sid-shakthivel <sid.shakthivel@gmail.com>
2022-07-13 22:01:03 +0100

    cleaned code

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -62,9 +62,3 @@ fn panic(info: &PanicInfo) -> ! {
     print!("Error: {}", info);
     loop {}
 }
-
-// Bochs magic breakpoint is xchg bx, bx
-// Dart: git clone https://kernel.googlesource.com/pub/scm/utils/dash/dash 
-
-// https://fejlesztek.hu/create-a-fat-file-system-image-on-linux/
-// https://stackoverflow.com/questions/22385189/add-files-to-vfat-image-without-mounting/29798605#29798605
---
+++ notes.md
@@ -0,0 +1,7 @@
+### List of useful resources
+
+Bochs magic breakpoint is xchg bx, bx
+Dart: git clone https://kernel.googlesource.com/pub/scm/utils/dash/dash 
+
+https://fejlesztek.hu/create-a-fat-file-system-image-on-linux/
+https://stackoverflow.com/questions/22385189/add-files-to-vfat-image-without-mounting/29798605#29798605
